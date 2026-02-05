7401da613f7ad0bc4f9fc66be66ae73a50a6a519
joao_junio <joaojunio818@gmail.com>
2026-02-05 16:18:06 -0300

    Ajustando documentação de PostControllerDocs

--- src/main/java/br/com/joaojuniodev/blog/controllers/docs/PostControllerDocs.java
+++ src/main/java/br/com/joaojuniodev/blog/controllers/docs/PostControllerDocs.java
@@ -3,6 +3,7 @@ package br.com.joaojuniodev.blog.controllers.docs;
 import br.com.joaojuniodev.blog.controllers.PostController;
 import br.com.joaojuniodev.blog.data.dto.model.PostDTO;
 import br.com.joaojuniodev.blog.data.dto.storage.StoredFileResponse;
+import br.com.joaojuniodev.blog.mediatype.MediaTypes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,8 +11,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -71,19 +70,25 @@ public interface PostControllerDocs {
     ResponseEntity<PostDTO> create(PostDTO postDTO);
 
     @Operation(
-            tags = {"Post"},
-            summary = "Create Post",
-            description = "Create Post",
-            responses = {
-                    @ApiResponse(
-                            description = "Success",
-                            responseCode = "200",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
-                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
-            }
+        tags = {"Post"},
+        summary = "Upload Image from Post",
+        description = "Upload Image from Post",
+        responses = {
+            @ApiResponse(
+                description = "Success",
+                responseCode = "200",
+                content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE),
+                    @Content(mediaType = MediaType.APPLICATION_XML_VALUE),
+                    @Content(mediaType = MediaTypes.APPLICATION_YAML)
+                }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+            @ApiResponse(description = "Not Acceptable", responseCode = "406", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
     )
     ResponseEntity<StoredFileResponse> uploadImageFromPost(MultipartFile image, Long postId);
 
